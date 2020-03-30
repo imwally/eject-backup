eb0cf66ba5868f8a2f245ee71a1a0e93a2d5eba9
Wally Jones <wally@wallyjones.com>
2020-03-29 23:21:53 -0400

    Add script

---
+++ eject-backup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+VOLUME_PATH="/Volumes/Backup"
+TM_RUNNING=$(tmutil status | grep "Running = 1;")
+
+
+# Don't eject if Time Machine is running
+if [ ! -z "$TM_RUNNING" ];
+then
+    osascript -e 'display notification "Time Machine is currently running. Ejection canceled." with title "🕓 Eject Backup"'
+    exit 1
+fi
+
+
+# Otherwise eject backup
+[ -d $VOLUME_PATH ] &&
+    diskutil unmount $VOLUME_PATH &&
+    osascript -e 'display notification "Backup disk successfully ejected." with title "✅ Eject Backup"' ||
+	osascript -e 'display notification "No backup disk found." with title "🚫 Eject Backup"'
+	
+
+
+
+
+
+    
